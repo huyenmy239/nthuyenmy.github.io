a916e71ef35c35f9190ab6979090339ff1a10f89
huyenmy239 <92309591+huyenmy239@users.noreply.github.com>
2021-10-20 13:40:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
+
+var typed = new Typed(".typing1", {
+    strings: ["Youtuber", "Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+
+var typed = new Typed(".typing2", {
+    strings: ["Youtuber", "Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
